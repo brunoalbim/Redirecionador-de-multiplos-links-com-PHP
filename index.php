ecdffc8c88fd251f9b6ba8d7d31e307b6efd830d
brunoalbim <bruno_albim@hotmail.com>
2023-01-19 00:29:55 +0000

    Defina o fuso horário dos logs

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ $arrayDeLinks = [
 require_once('functions.php');
 
 // Registro de log
-//registro_log(date('d/m/Y').', '.date('H:i:s').', '.$gerarLink);
+date_default_timezone_set('America/Sao_Paulo');
+registro_log(date('d/m/Y').', '.date('H:i:s').', '.$gerarLink);
 
 // Abaixo crie a sua lógica:
 echo "Redireciona para: ". $gerarLink;
